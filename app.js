af4a3b6aafd603a7d84455b21d6e8e499d2f1ee4
brucec0379 <47831134+brucec0379@users.noreply.github.com>
2022-11-30 23:00:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-import express from 'express'
-import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/tuiter);
+import express from 'express';
 import cors from 'cors'
-import HelloController
-    from "./controllers/hello-controller.js";
-import UserController
-    from "./controllers/users/users-controller.js";
-import TuitsController
-    from "./controllers/tuits/tuits-controller.js";
-const app = express();
-app.use(cors())
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: true
+}))
 app.use(express.json());
 TuitsController(app);
-HelloController(app);
-UserController(app);
-app.listen(process.env.PORT ||4000)
-
-const CONNECTION_STRING = 'mongodb+srv://Chenghai0379:LoveLzj1314@cluster0.y0wpi29.mongodb.net/?retryWrites=true&w=majority
-    || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000)
